762629498bb45b887123e406a74a44cb4e81d45a
imranwebcraft <imranhossainsakil22@gmail.com>
2024-01-13 23:34:40 +0600

    Winner calculate function added and after win the game over message will be shown if user try to continue the game

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
-import { useState } from 'react';
-import toast from 'react-hot-toast';
+import { useState } from "react";
+import toast from "react-hot-toast";
 
 // Square component
 function Square({ value, onSquareClick }) {
@@ -17,24 +17,38 @@ export default function Board() {
 	const [square, setSquare] = useState(Array(9).fill(null));
 	const [xISNext, setXIsNext] = useState(true);
 
+	const winner = calculateWinner(square);
+	let status;
+	if (winner) {
+		status = `Winner: ${winner}`;
+	} else {
+		status = "Next player: " + (xISNext ? "X" : "O");
+	}
+
 	function handleSquareClick(index) {
 		const nextSquare = square.slice();
 
 		if (square[index]) {
-			return toast.error('Alreay played! Try another square.');
+			return toast.error("Alreay played! Try another square.");
+		}
+		if (calculateWinner(square)) {
+			return toast.error("Gamer Over!");
 		}
 
 		if (xISNext) {
-			nextSquare[index] = 'X';
+			nextSquare[index] = "X";
 		} else {
-			nextSquare[index] = 'O';
+			nextSquare[index] = "O";
 		}
+
 		setSquare(nextSquare);
 		setXIsNext(!xISNext);
 	}
 
 	return (
-		<div className="flex flex-col justify-center items-center h-screen w-screen">
+		<div className="flex flex-col justify-center h-screen w-screen">
+			<h1 className="text-2xl text-center pb-5 font-medium">{status}</h1>
+
 			<div className="flex justify-center items-center">
 				<Square value={square[0]} onSquareClick={() => handleSquareClick(0)} />
 				<Square value={square[1]} onSquareClick={() => handleSquareClick(1)} />
@@ -52,4 +66,24 @@ export default function Board() {
 			</div>
 		</div>
 	);
+}
+
+function calculateWinner(squares) {
+	const lines = [
+		[0, 1, 2],
+		[3, 4, 5],
+		[6, 7, 8],
+		[0, 3, 6],
+		[1, 4, 7],
+		[2, 5, 8],
+		[0, 4, 8],
+		[2, 4, 6],
+	];
+	for (let i = 0; i < lines.length; i++) {
+		const [a, b, c] = lines[i];
+		if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+			return squares[a];
+		}
+	}
+	return null;
 }
