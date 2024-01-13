ce2b7e679c7deb09338e32e82b4939827c72271a
imranwebcraft <imranhossainsakil22@gmail.com>
2024-01-13 22:56:08 +0600

    assign different value for square and pass the index to handler function to change to assign X for click

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import { useState } from 'react';
 
 // Square component
-function Square({ value }) {
+function Square({ value, onSquareClick }) {
 	return (
-		<button className=" w-20 h-20 border border-gray-100 bg-gradient-to-r from-sky-400 to-blue-500 text-white text-2xl rounded m-1 leading-9 hover:opacity-85  transition-all duration-150">
+		<button
+			onClick={onSquareClick}
+			className=" w-20 h-20 border border-gray-100 bg-gradient-to-r from-sky-400 to-blue-500 text-white text-2xl rounded m-1 leading-9 hover:opacity-85  transition-all duration-150"
+		>
 			{value}
 		</button>
 	);
@@ -12,22 +15,28 @@ function Square({ value }) {
 export default function Board() {
 	const [square, setSquare] = useState(Array(9).fill(null));
 
+	function handleSquareClick(index) {
+		const nextSquare = square.slice();
+		nextSquare[index] = 'X';
+		setSquare(nextSquare);
+	}
+
 	return (
 		<div className="flex flex-col justify-center items-center h-screen w-screen">
 			<div className="flex justify-center items-center">
-				<Square value={square[0]} />
-				<Square value={square[1]} />
-				<Square value={square[2]} />
+				<Square value={square[0]} onSquareClick={() => handleSquareClick(0)} />
+				<Square value={square[1]} onSquareClick={() => handleSquareClick(1)} />
+				<Square value={square[2]} onSquareClick={() => handleSquareClick(2)} />
 			</div>
 			<div className="flex justify-center items-center">
-				<Square value={square[3]} />
-				<Square value={square[4]} />
-				<Square value={square[5]} />
+				<Square value={square[3]} onSquareClick={() => handleSquareClick(3)} />
+				<Square value={square[4]} onSquareClick={() => handleSquareClick(4)} />
+				<Square value={square[5]} onSquareClick={() => handleSquareClick(5)} />
 			</div>
 			<div className="flex justify-center items-center">
-				<Square value={square[6]} />
-				<Square value={square[7]} />
-				<Square value={square[8]} />
+				<Square value={square[6]} onSquareClick={() => handleSquareClick(6)} />
+				<Square value={square[7]} onSquareClick={() => handleSquareClick(7)} />
+				<Square value={square[8]} onSquareClick={() => handleSquareClick(8)} />
 			</div>
 		</div>
 	);
