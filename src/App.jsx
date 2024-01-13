fb188588a6da8ef4e7bbb1d8aa7011bcb6450394
imranwebcraft <imranhossainsakil22@gmail.com>
2024-01-13 20:22:32 +0600

    Game board and square component added

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,31 @@
-export default function App() {
+// Square component
+
+function Square() {
+	return (
+		<button className=" w-20 h-20 border border-gray-100 bg-indigo-500 text-white text-2xl rounded m-1 leading-9">
+			X
+		</button>
+	);
+}
+
+export default function Board() {
 	return (
 		<div>
-			<h1 className=" text-3xl font-bold">Hello Tic Tac Toe</h1>
+			<div>
+				<Square />
+				<Square />
+				<Square />
+			</div>
+			<div>
+				<Square />
+				<Square />
+				<Square />
+			</div>
+			<div>
+				<Square />
+				<Square />
+				<Square />
+			</div>
 		</div>
 	);
 }
